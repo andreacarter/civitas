7116859ed62447984c77df1f86b7137b53684415
Andrea Carter <40808764+mirtacarter@users.noreply.github.com>
2018-09-20 13:05:19 -0400

    sidebar nav links and icons updated

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -2,7 +2,7 @@
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
 // import ContentPaste from "@material-ui/icons/ContentPaste";
-import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import MapOutlined from "@material-ui/icons/MapOutlined";
 import BubbleChart from "@material-ui/icons/BubbleChart";
 import LocationOn from "@material-ui/icons/LocationOn";
 import Notifications from "@material-ui/icons/Notifications";
@@ -20,35 +20,35 @@ const dashboardRoutes = [
   {
     path: "/dashboard",
     sidebarName: "Dashboard",
-    navbarName: "Material Dashboard",
+    navbarName: "Civitas Dashboard",
     icon: Dashboard,
     component: DashboardPage
   },
   {
     path: "/user",
-    sidebarName: "User Profile",
-    navbarName: "Profile",
+    sidebarName: "Bank Profile",
+    navbarName: "Bank Profile",
     icon: Person,
     component: UserProfile
   },
   {
     path: "/table",
-    sidebarName: "Table List",
-    navbarName: "Table List",
+    sidebarName: "Local Organizations",
+    navbarName: "Local Organizations",
     icon: "content_paste",
     component: TableList
   },
   {
     path: "/typography",
-    sidebarName: "Typography",
-    navbarName: "Typography",
-    icon: LibraryBooks,
+    sidebarName: "Branch Locations",
+    navbarName: "Branch Locations",
+    icon: LocationOn,
     component: Typography
   },
   {
     path: "/icons",
-    sidebarName: "Icons",
-    navbarName: "Icons",
+    sidebarName: "Goals",
+    navbarName: "Goals",
     icon: BubbleChart,
     component: Icons
   },
@@ -56,7 +56,7 @@ const dashboardRoutes = [
     path: "/maps",
     sidebarName: "Maps",
     navbarName: "Map",
-    icon: LocationOn,
+    icon: MapOutlined,
     component: Maps
   },
   {
