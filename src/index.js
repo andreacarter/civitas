88547658899240b0980030fd1b650ab824c9d00d
Andrea Carter <40808764+mirtacarter@users.noreply.github.com>
2018-09-20 23:47:58 -0400

    landing page

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import { Router, Route, Switch } from "react-router-dom";
 
 import "assets/css/material-dashboard-react.css?v=1.4.1";
 
-import indexRoutes from "routes/index.jsx";
+// import indexRoutes from "routes/index.jsx";
 
-const hist = createBrowserHistory();
+// const hist = createBrowserHistory();
 
-ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} component={prop.component} key={key} />;
-      })}
-    </Switch>
-  </Router>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <Router history={hist}>
+//     <Switch>
+//       {indexRoutes.map((prop, key) => {
+//         return <Route path={prop.path} component={prop.component} key={key} />;
+//       })}
+//     </Switch>
+//   </Router>,
+//   document.getElementById("root")
+// );
